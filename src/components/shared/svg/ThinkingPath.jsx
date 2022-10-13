9f6508a540c8c221f92b82a7cfefd7435acb3b62
Julia <iulia.ryseva@gmail.com>
2022-10-13 10:16:13 +0300

    change start screen text size

--- src/components/shared/svg/ThinkingPath.jsx
+++ src/components/shared/svg/ThinkingPath.jsx
@@ -1,9 +1,8 @@
 export const ThinkingPath = (props) => (
-    <svg {...props} viewBox="0 0 246 129" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="203.5" cy="86.5" r="42.5" fill="#F4F4F5"/>
-        <circle cx="130.5" cy="29.5" r="29.5" fill="#F4F4F5"/>
-        <circle cx="59" cy="19" r="18" fill="#F4F4F5"/>
-        <circle cx="8.5" cy="39.5" r="8.5" fill="#F4F4F5"/>
+    <svg {...props} viewBox="0 0 91 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="73.4581" cy="26.2947" r="14.5" transform="rotate(-12.4352 73.4581 26.2947)" fill="#F4F4F5"/>
+        <circle cx="44.3574" cy="12.7431" r="10" transform="rotate(-12.4352 44.3574 12.7431)" fill="#F4F4F5"/>
+        <circle cx="20.059" cy="14.0051" r="6" transform="rotate(-12.4352 20.059 14.0051)" fill="#F4F4F5"/>
+        <circle cx="3.60811" cy="25.3128" r="2.5" transform="rotate(-12.4352 3.60811 25.3128)" fill="#F4F4F5"/>
     </svg>
-
 )
